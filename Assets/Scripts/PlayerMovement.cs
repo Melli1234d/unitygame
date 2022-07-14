fd9373abd0c6b671ae2bdc0e06998b391147b109
Melli1234d <58852401+Melli1234d@users.noreply.github.com>
2022-07-14 22:27:05 +0200

    game
    game

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,51 +1,84 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
-    [SerializeField] private float jumpspeed;
+    [SerializeField] private float jumpPower;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask wallLayer;
     private Rigidbody2D body;
     private Animator anim;
-    private bool grounded;
+    private BoxCollider2D boxCollider;
+    private float wallJumpCooldown;
 
     private void Awake()
     {
 
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
-
+        
         //Richtungswechsel links/rechts vom spieler
         if (horizontalInput > 0.05f)
             transform.localScale = new Vector2(5, 5);
         else if (horizontalInput < -0.05f)
             transform.localScale = new Vector2(-5, 5);
 
-        if (Input.GetKey(KeyCode.Space) && grounded)
-            Jump();
-
         //set animator Hinweis: name in klammern muss genau so geschrieben sein wie in animation window
         anim.SetBool("run", horizontalInput != 0);
-        anim.SetBool("grounded", grounded);
+        anim.SetBool("grounded", isGrounded());
+
+        //Wandspringen
+        if (wallJumpCooldown > 0.2f)
+        {
+            body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+            if (onWall() && !isGrounded())
+            {
+                body.gravityScale = 0;
+                body.velocity = Vector2.zero; //wenn er dran springt fällt er nicht
+            }
+            else
+                body.gravityScale = 7;
+        }
+        else
+            wallJumpCooldown += Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Space))
+            Jump();
     }
 
     private void Jump()
     {
-        body.velocity = new Vector2(body.velocity.x,jumpspeed);
-        anim.SetTrigger("jump");
-        grounded = false;
+        if (isGrounded())
+        {
+            body.velocity = new Vector2(body.velocity.x, jumpPower);
+            anim.SetTrigger("jump");
+        }
+        else if (onWall() && !isGrounded())
+        {
+            wallJumpCooldown = 0;
+            body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 3, 6); //- weil der spieler weg von der wand gedrückt werden soll
+        }  
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Ground")
-            grounded = true;
+    }
+
+    private bool isGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
+    private bool onWall()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x,0.1f), 0.1f, wallLayer);
+        return raycastHit.collider != null;
     }
 }
