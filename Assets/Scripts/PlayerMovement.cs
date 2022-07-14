4734c43ceda4d204bc0c1144c1e918dbb33addb6
Melli1234d <58852401+Melli1234d@users.noreply.github.com>
2022-07-14 13:04:59 +0200

    asset
    asset

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
+
+    private void Awake()
+    {
+        
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        //Richtungswechsel links/rechts vom spieler
+        if (horizontalInput > 0.05f)
+            transform.localScale = new Vector2(5, 5);
+        else if(horizontalInput < -0.05f)
+            transform.localScale = new Vector2(-5,5);
+
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
+
+        //set animator Hinweis: name in klammern muss genau so geschrieben sein wie in animation window
+        anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
+
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
+    }
+}
