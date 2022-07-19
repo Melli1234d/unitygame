6ab27160b0e241cc75ce480087473c7bb430c467
Melli1234d <58852401+Melli1234d@users.noreply.github.com>
2022-07-19 13:35:25 +0200

    game
    game

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,13 +6,18 @@ public class CameraController : MonoBehaviour
     [SerializeField] private float speed;
     private float currentPosX;
     private Vector3 velocity = Vector3.zero;
+    [SerializeField] private Transform player;
 
     private void Update()
     {
-        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y,transform.position.z),
-            ref velocity, speed* Time.deltaTime);
+        // follow room
+        // transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
+
+        transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
+
     }
 
+    
     public void MoveToNewRoom(Transform _newRoom)
     {
         currentPosX = _newRoom.position.x;
